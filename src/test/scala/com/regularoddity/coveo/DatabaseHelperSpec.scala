bbea33780cda59988833185a424039fa9375affb
Emmanuel Eytan <eje211@gmail.com>
2018-11-20 00:16:17 -0500

    Added a few tests.

--- src/test/scala/com/regularoddity/coveo/DatabaseHelperSpec.scala
+++ src/test/scala/com/regularoddity/coveo/DatabaseHelperSpec.scala
@@ -1,5 +1,16 @@
 package com.regularoddity.coveo
 
-class DatabaseHelperSpec {
+import org.scalatest.{Matchers, WordSpec}
 
+class DatabaseHelperSpec extends WordSpec with Matchers with DatabaseHelpers {
+  "The database helper" can {
+    "the fuzzy string maker" should {
+      "add percent signs between each character of a string" in {
+        fuzzyString("example") shouldBe "e%x%a%m%p%l%e"
+      }
+      "keep an empty string empty" in {
+        fuzzyString("") shouldBe ""
+      }
+    }
+  }
 }
